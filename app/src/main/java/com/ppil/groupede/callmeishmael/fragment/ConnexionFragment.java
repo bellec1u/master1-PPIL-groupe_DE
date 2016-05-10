2b96ed0f4f5a5e4e7eee8fc04715a0208c852759
Pima <Pima@macbook.home>
2016-05-10 18:21:20 +0200

    ajout listener connexion

--- app/src/main/java/com/ppil/groupede/callmeishmael/fragment/ConnexionFragment.java
+++ app/src/main/java/com/ppil/groupede/callmeishmael/fragment/ConnexionFragment.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ppil.groupede.callmeishmael.R;
 
@@ -15,7 +19,9 @@ import com.ppil.groupede.callmeishmael.R;
  */
 public class ConnexionFragment extends Fragment {
 
-
+    private Button logIn;
+    private AutoCompleteTextView email;
+    private EditText password;
     public ConnexionFragment() {
         // Required empty public constructor
     }
@@ -25,7 +31,23 @@ public class ConnexionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_connexion, container, false);
+        View view = inflater.inflate(R.layout.fragment_connexion, container, false);
+        logIn = (Button) view.findViewById(R.id.action_sign_in);
+        email = (AutoCompleteTextView) view.findViewById(R.id.email);
+        password = (EditText) view.findViewById(R.id.password);
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailS, pwdS;
+                emailS = email.getText().toString();
+                pwdS = password.getText().toString();
+                if (emailS.equals("") || pwdS.equals("")) {
+                    Toast.makeText(getActivity(), "Champ(s) vide(s) !", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+        return view;
     }
 
 }
