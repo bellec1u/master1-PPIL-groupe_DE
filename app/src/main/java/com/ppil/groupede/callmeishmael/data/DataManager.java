4705190fd6eea3f9e7910cc3b37f2a0bc9b4fbf5
Pima <Pima@macbook.home>
2016-05-10 15:48:58 +0200

    data manager TODO

---
+++ app/src/main/java/com/ppil/groupede/callmeishmael/data/DataManager.java
@@ -0,0 +1,13 @@
+package com.ppil.groupede.callmeishmael.data;
+
+import android.app.ActivityManager;
+
+/**
+ * Created by Pima on 10/05/16.
+ */
+public class DataManager implements Runnable {
+    @Override
+    public void run() {
+        
+    }
+}
