656cb7b48eb6ad0d61b041cfcc28de5368b0d282
Léopold Bellec <Leopold@pc28.home>
2016-05-11 17:43:17 +0200

    Ajout suggestion + bare de séparation des parties + autres (me souvient plus)

--- app/src/main/java/com/ppil/groupede/callmeishmael/fragment/AccueilFragment.java
+++ app/src/main/java/com/ppil/groupede/callmeishmael/fragment/AccueilFragment.java
@@ -35,11 +35,50 @@ public class AccueilFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_accueil, container, false);
 
-        int nbLivre = 14;
+        // ---------- ---------- ---------- ---------- ---------- Liste de suggestion
+
+        int nbLivre = 8;
         int nbColonnes = 3;
         int nbLignes = ((int)(nbLivre/3)) + 1;
 
-        GridLayout gl = (GridLayout) view.findViewById(R.id.liste_de_lecture);
+        GridLayout gl = (GridLayout) view.findViewById(R.id.liste_des_suggestions);
+        gl.removeAllViews();
+        gl.setColumnCount( nbColonnes );
+        gl.setRowCount( nbLignes );
+
+        for (int i = 0, c = 0, r = 0; i < nbLivre; i++, c++) {
+            if (c == nbColonnes) {
+                c = 0;
+                r++;
+            }
+
+            ImageButton ib = new ImageButton(view.getContext());
+
+            int largeur = (int) (100 * getResources().getDisplayMetrics().density + 0.5f);
+            int hauteur = (int) (150 * getResources().getDisplayMetrics().density + 0.5f);
+
+            ib.setMinimumWidth(largeur);
+            ib.setMinimumHeight(hauteur);
+            ib.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View arg0) {
+
+                    Toast.makeText(getActivity(), "ImageButton is clicked!", Toast.LENGTH_SHORT).show();
+
+                }
+
+            });
+
+            GridLayout.LayoutParams gridParam = new GridLayout.LayoutParams(GridLayout.spec(r), GridLayout.spec(c));
+            gl.addView(ib, gridParam);
+        }
+
+        // ---------- ---------- ---------- ---------- ---------- Liste de lecture
+        nbLivre = 4;
+        nbLignes = ((int)(nbLivre/3)) + 1;
+
+        gl = (GridLayout) view.findViewById(R.id.liste_de_lecture);
         gl.removeAllViews();
         gl.setColumnCount( nbColonnes );
         gl.setRowCount( nbLignes );
