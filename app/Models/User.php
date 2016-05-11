fb08db132d769d31d547e936f58396eed6862ee3
laurent bouquin <laurent bouquin>
2016-05-11 11:48:51 +0200

    deplacement user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,10 +1,31 @@
 <?php
 
-namespace App\models;
+namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+         'email', 'first_name', 'last_name', 'password', 'sex', 'birth_date', 'profile_image'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    
+public function setPasswordAttribute($password)
+{
+    $this->attributes['password'] = bcrypt($password);
+}
 }
