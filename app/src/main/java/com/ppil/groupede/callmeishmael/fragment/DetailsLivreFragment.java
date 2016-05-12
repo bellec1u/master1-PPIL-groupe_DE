504268a9abc7eb1a2148933965ff3ba763943daa
Pima <Pima@macbook.home>
2016-05-12 15:12:21 +0200

    vue detail, ok

---
+++ app/src/main/java/com/ppil/groupede/callmeishmael/fragment/DetailsLivreFragment.java
@@ -0,0 +1,31 @@
+package com.ppil.groupede.callmeishmael.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ppil.groupede.callmeishmael.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetailsLivreFragment extends Fragment {
+
+
+    public DetailsLivreFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details_livre, container, false);
+    }
+
+}
