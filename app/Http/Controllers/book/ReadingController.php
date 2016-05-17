074257220f8c8f6576ff73198b32778143333565
Hturi <ghaniturismo@gmail.com>
2016-05-17 22:18:11 +0200

    add ReadingController and ReadingCreateRequest

---
+++ app/Http/Controllers/book/ReadingController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\book;
+
+
+use App\Http\Requests\book\ReadingCreateRequest;
+use App\Repositories\book\ReadingRepository;
+use App\Http\Requests;
+use App\Repositories\BookRepository;
+use App\Http\Controllers\Controller;
+
+class ReadingController extends Controller
+{
