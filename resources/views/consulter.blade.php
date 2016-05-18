bd694803235abd4714846de92bfee7262a4d3ddd
Hturi <ghaniturismo@gmail.com>
2016-05-18 11:34:33 +0200

    add view consulter ma biblio [VIDE]

---
+++ resources/views/consulter.blade.php
@@ -0,0 +1,25 @@
+@extends('user.template')
+
+@section('titre')
+    Call Me Ishmael | Référencement et Lecture d&#039;Ebooks
+@stop
+
+@section('contenu')
+    <div class="container">
+        <div class="panel panel-info">
+            <div class="panel-heading">La liste de ma lecture en cours </div>
+            <div class="panel-body">
+                
+                <br /><br /><br /><br /><br /><br /><br /><br />
+                <?php 
+                    /*
+                    <p><a href="{{ url('book/4') }}"><img src="{{ $book->cover_url  }}" alt="" /></a></p>
+                    <br /><br /><br /><br /><br /><br /><br /><br />
+                    <hr />
+                    <span style="text-decoration: underline;font-weight: bold;">&Agrave; Découvrir</span> :<br />
+                    */
+                ?>
+            </div>
+        </div>
+    </div>
+@stop
