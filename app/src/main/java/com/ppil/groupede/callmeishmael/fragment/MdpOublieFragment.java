48748641357dc98c74a135a94daae13dafa3143b
Chabaux <paul.chabaux@gmail.com>
2016-05-18 20:14:31 +0200

    finition vue mdp oublie

--- app/src/main/java/com/ppil/groupede/callmeishmael/fragment/MdpOublieFragment.java
+++ app/src/main/java/com/ppil/groupede/callmeishmael/fragment/MdpOublieFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.ppil.groupede.callmeishmael.R;
 
@@ -13,8 +15,12 @@ import com.ppil.groupede.callmeishmael.R;
  */
 public class MdpOublieFragment extends Fragment {
 
+    private Button ok;
+    private EditText email;
+
     public MdpOublieFragment(){
         // required empty constructor
+
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,7 +30,17 @@ public class MdpOublieFragment extends Fragment {
             On récupère les élements à partir de la view
             et on les affecte à l'attribut de classe correspondant.
          */
+
         View view = inflater.inflate(R.layout.fragment_mdp_oublie, container, false);
+
+        ok = (Button) view.findViewById(R.id.button_ok);
+        email = (EditText) view.findViewById(R.id.editText_mail);
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //REQUETE EMAIL MDP OUBLIE
+                }});
 
         return view;
     }
