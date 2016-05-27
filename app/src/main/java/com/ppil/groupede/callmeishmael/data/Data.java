bee4532346200422384d2a2a480b631df508680a
Pima <Pima@iPima54.local>
2016-05-27 12:23:00 +0200

    test dl et test import ok

--- app/src/main/java/com/ppil/groupede/callmeishmael/data/Data.java
+++ app/src/main/java/com/ppil/groupede/callmeishmael/data/Data.java
@@ -27,7 +27,7 @@ public class Data {
     public String path;
 
     private Data() {
-        ipMachine = "http://192.168.212.157";
+        ipMachine = "http://192.168.43.7";
         port = "8888";
         adresse = ipMachine + ":" + port;
 
